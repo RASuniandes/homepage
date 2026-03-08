400f80e8be776872d51a3eb00983b1bde2b2d7a8
Ivan Gomez <ivandavidgomezsilva@hotmail.com>
2026-03-08 01:18:46 -0500

    Fix LinkedIn link to point to company page instead of posts feed

--- src/components/layout/bubbles.tsx
+++ src/components/layout/bubbles.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function Bubbles() {
   return <>
     <Link
-      to="https://www.linkedin.com/company/rasuniandes/posts/?feedView=all"
+      to="https://www.linkedin.com/company/rasuniandes"
       className="fixed bottom-4 right-4 w-10 h-10 overflow-hidden rounded-full border-white z-100">
       <Linkedin className="w-10 h-10 bg-blue-400 p-2 text-white" />
     </Link>
